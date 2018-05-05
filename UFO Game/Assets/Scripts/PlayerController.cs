9f61207d9837faf3dfa82805e554c0c3be215a70
Katy Rumble <rumblec@sunyulster.edu>
2018-05-05 17:20:09 -0400

    Completed 7 of 9

--- UFO Game/Assets/Scripts/PlayerController.cs
+++ UFO Game/Assets/Scripts/PlayerController.cs
@@ -27,4 +27,14 @@ public class PlayerController : MonoBehaviour {
 		rb2d.AddForce (movement * speed);
 
 	}
+
+	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
 }
